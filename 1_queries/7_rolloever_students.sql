9d4a3e1e49be8c238ccd77596ffd5b3ea058dd59
Billy Wong <bwong.yh@protonmail.com>
2022-06-03 19:22:17 +0900

    use JOIN to display rollover students

---
+++ 1_queries/7_rolloever_students.sql
@@ -0,0 +1,4 @@
+SELECT students.name as student_name, cohorts.name as cohort_name, students.start_date as student_start_date, cohorts.start_date as cohorts_start_date FROM students 
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE students.start_date <> cohorts.start_date
+  ORDER BY cohorts.start_date
